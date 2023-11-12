f42a4d0d06221ba543a4f1fc2827eb2adb55adb3
Francisco Junior <francisco.oliveira>
2023-11-12 09:05:51 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Manutenção</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600&family=Nunito:wght@400;700&family=Open+Sans:wght@300;400&family=Roboto:ital,wght@0,100;0,300;0,400;1,300&display=swap" rel="stylesheet">
+  <style>
+    * {
+      font-family: 'Open Sans', sans-serif;
+    }
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      flex-direction: column;
+    }
+
+    .title {
+      font-size: 5rem;
+      color: #8B97A2;
+      font-weight: bold;
+      text-align: center;
+
+      & p {
+        font-size: 1.5rem;
+        font-weight: normal;
+        line-height: .8;
+        & span {
+          color: #00A335
+        }
+      }
+    }
+
+    .copy {
+      font-size: 1rem;
+      color: #8B97A2;
+      font-weight: normal;
+      text-align: center;
+      margin-top: 2rem;}
+  </style>
+</head>
+<body>
+  <div class="title">
+    Em manutenção
+    <p>Nossos servidores estão sendo atualizados</p>
+    <p>O site e/ou serviços poderão ficar indisponíveis temporariamente</p>
+    <p><span>Agradecemos a compreensão</span></p>
+  </div>
+  <div class="copy">© 2023 - Intelbras S/A</div>
+</body>
+</html>
